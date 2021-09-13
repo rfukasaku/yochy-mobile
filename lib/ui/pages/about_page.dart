6030fd08e79cf0df68d59f3570b75fd0b3c72a19
fukky21 <r.fukky21@gmail.com>
2021-09-14 05:20:46 +0900

    fix: 軽微な修正

--- lib/ui/pages/about_page.dart
+++ lib/ui/pages/about_page.dart
@@ -7,6 +7,8 @@ class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
   static const route = '/about';
   static const url = 'https://yochy-cea25.web.app/about/?from=mobile';
+  static const contactFormUrl =
+      'https://docs.google.com/forms/d/e/1FAIpQLSetLQIcyNt628Er-HqRlPGZKZP97a095m7O8lvtUNO2dv4S8Q/viewform?usp=sf_link';
 
   @override
   Widget build(BuildContext context) {
@@ -19,9 +21,8 @@ class AboutPage extends StatelessWidget {
           await controller.stopLoading();
           final action = url.toString().replaceAll('yochy-mobile:', '');
           if (action == 'openContactForm') {
-            const url = 'https://docs.google.com/forms/d/e/1FAIpQLSetLQIcyNt628Er-HqRlPGZKZP97a095m7O8lvtUNO2dv4S8Q/viewform?usp=sf_link';
-            if (await canLaunch(url)) {
-              await launch(url);
+            if (await canLaunch(contactFormUrl)) {
+              await launch(contactFormUrl);
             } else {
               showErrorModal(context, 'エラーが発生しました');
             }
